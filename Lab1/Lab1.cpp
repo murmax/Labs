bdf14cc4ce8c166d36cbe1e22263c8651c75ccb8
murmax <maxim_mour@mail.ru>
2018-03-14 13:23:01 +0300

    Update Lab1.cpp

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -1,27 +1,85 @@
-#define WIDTH 80
 #include <iostream>
 
-int main()
+#define F 2
+
+void print(unsigned int* arr, unsigned int N, int k)
+
 {
-    int n, k;
-    std::cout<<"Enter amount of pages: "<<std::endl; std::cin>>n;
 
+for (int i=0;i<N;i++)
 
-    do{
-    std::cout<<std::endl;
-    std::cout<<"Enter which page is opened now: "<<std::endl; std::cin>>k;
+{
 
-    if (k>3)     std::cout<<"1 ";
-    if (k>4)     std::cout<<".. ";
-    if (k>2)     std::cout<<k-2<<" ";
-    if (k>1)     std::cout<<k-1<<" ";
-    std::cout<<"["<<k<<"] ";
-    if (k<n)     std::cout<<k+1<<" ";
-    if (k<n-1)   std::cout<<k+2<<" ";
-    if (k<n-3)   std::cout<<".. ";
-    if (k<n-2)   std::cout<<n;
-    } while((k>0)&&(k<=n));
-    std::cout<<std::endl<<"Over";
+if (arr[i]==0)
 
-    return 0;
+std::cout<<".. ";
+
+else if (arr[i] == k)
+
+std::cout<<"["<<arr[i]<<"] ";
+
+else
+
+std::cout<<arr[i]<<" ";
+
+}
+
+}
+
+int main()
+
+{
+
+using std::cout;
+
+using std::endl;
+
+using std::cin;
+
+int n,k;
+
+cout<<"Enter N: ";cin>>n;
+
+unsigned int outarr[5+2*F];
+
+unsigned char step;
+
+do{
+
+step = 0;
+
+cout<<endl<<"Enter K: ";cin>>k;
+
+cout<<endl;
+
+if ((k<=0)||(k>n)) break;
+
+if (k>1+F) outarr[step++] = 1;//cout<<"1 ";
+
+if (k>2+F) outarr[step++] = 0;//cout<<".. ";
+
+for (int i=k-F;i<=k;i++)
+
+if (i>0)
+
+outarr[step++] = i;
+
+for (int i=k+1;i<=k+F;i++)
+
+if (i<=n)
+
+outarr[step++] = i;
+
+if (k<n-F-1) outarr[step++] = 0;//cout<<".. ";
+
+if (k<n-F) outarr[step++] = n;//cout<<n;
+
+print(outarr,step,k);
+
+} while (1);
+
+cout<<"GAME OVER ";
+
+return 0;
+
 }
